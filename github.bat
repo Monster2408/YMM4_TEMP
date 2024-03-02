c6aeaa6ab7f9f99d69f76b3515ffce6ad23d866d
Monster2408 <meoto2408@gmail.com>
2024-03-02 13:52:24 +0900

    commit

--- github.bat
+++ github.bat
@@ -1,21 +1,11 @@
 @echo off
-
 git add .
-
-echo.
-echo ＞＞ コミットメッセージを入力してください。
-echo.
+echo input commit message
 set /p input=
 if "%input%" == "" (
 	git commit -am "this is update commit"
 ) else (
 	git commit -am "%input%"
 )
-echo.
-echo ＞＞ コミットが完了しました。
-echo.
 git push origin master
-echo.
-echo ＞＞ プッシュが完了しました。
-echo.
 pause
